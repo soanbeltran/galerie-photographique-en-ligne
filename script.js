3d8bd98ce1b138dfb9bd27fffa93cbdfa73415bb
soanBeltran photo <beltransoan@gmail.com>
2025-04-16 16:43:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,3 +53,17 @@ function processPayment(event) {
   closePayment();
   clearCart();
 }
+// Animation au scroll
+const sections = document.querySelectorAll('.section');
+
+function revealSections() {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < window.innerHeight - 100) {
+      section.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealSections);
+window.addEventListener('load', revealSections);
